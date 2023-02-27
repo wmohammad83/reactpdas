a3adabe6e368635a80ce028b46e2f4f94dd81994
Waseem Mohammad <wmohammad83@icloud.com>
2023-02-27 20:07:24 +0000

    initial commit and app setup

---
+++ pdas/src/App.js
@@ -0,0 +1,15 @@
+import ProfileCard from "./ProfileCard"
+
+function App() {
+    return (
+        <div>
+            <div>Personal Digital assistance</div>
+
+            < ProfileCard />
+            < ProfileCard />
+            < ProfileCard />
+        </div>
+    );
+}
+
+export default App;
